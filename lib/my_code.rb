3ac8205fce1b4c6501ec28502349ccfe6f53bc5d
Sohaib Mubashir <sohaibmh@yahoo.com>
2019-07-10 21:41:07 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,30 @@
-# My Code here....
+
+def map_to_negativize(array)
+  array.map {|x| x * -1}
+end
+
+def map_to_no_change(array)
+  array.map { |x| x }
+end
+
+def map_to_double(array)
+  array.map {|x| x * 2}
+end
+
+def map_to_square(array)
+  array.map {|x| x * x}
+end
+
+
+def reduce_to_total(array, starting_point = 0)
+array.reduce(starting_point) {|sum, n| sum + n}
+end
+
+def reduce_to_all_true(array)
+  array.reduce{|e, f| e && f}
+end
+
+
+def reduce_to_any_true(array)
+  array.reduce{|e, f| e or !!f}
+end
