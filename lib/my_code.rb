19e16133d28faa927248e8cba16de203b5cfb152
Sohaib Mubashir <sohaibmh@yahoo.com>
2019-07-10 21:12:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,12 +15,9 @@ def map_to_square(array)
   array.map {|x| x * x}
 end
 
-def reduce_to_total(array)
-array.reduce() {|sum, n| sum + n}
-end
 
 def reduce_to_total(array)
-array.reduce(11) {|sum, n| sum + n}
+array.reduce() {|sum, n| sum + n}
 end
 
 def reduce_to_all_true(array)
