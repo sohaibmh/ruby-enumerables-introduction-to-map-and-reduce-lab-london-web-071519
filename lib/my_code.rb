fad4be586a681eed7f6b1386e0a48317d795b38a
Sohaib Mubashir <sohaibmh@yahoo.com>
2019-07-10 21:10:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# My Code here....
+
+def map_to_negativize(array)
+  array.map {|x| x * -1}
+end
+
+def map_to_no_change(array)
+  array.map { |x| x }
+end
+
+def map_to_double(array)
+  array.map {|x| x * 2}
+end
+
+def map_to_square(array)
+  array.map {|x| x * x}
+end
+
+def reduce_to_total(array)
+array.reduce(0) {|sum, n| sum + n}
+end
+
+def reduce_to_all_true(array)
+  array.reduce(100) {|sum, n| sum + n }
+end
